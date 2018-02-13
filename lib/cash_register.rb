5149f56fd1a484e9caebfbe299fe36f42556d7bb
Michael Kang <michaelkang85@gmail.com>
2018-02-13 01:05:40 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
     self.total += price * quantity
   end
 
-  def apply_discount(discount)
+  def apply_discount
     if self.discount == 0
       puts "There is no discount to apply"
     else
