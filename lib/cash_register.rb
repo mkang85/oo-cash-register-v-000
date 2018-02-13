d27a175aa69de2518e483d445b58e85b8442c667
Michael Kang <michaelkang85@gmail.com>
2018-02-13 00:25:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,13 +3,9 @@ class CashRegister
 
   attr_accessor :total, :discount
 
-  def initialize(total, discount)
+  def initialize(discount)
     @total = 0
     @discount = discount
-  end
-
-  def total
-    @total
   end
 
 
