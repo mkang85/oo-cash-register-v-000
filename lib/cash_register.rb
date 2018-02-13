99412a60b5ff85c6f05f85994ca0ecbf23b94cf7
Michael Kang <michaelkang85@gmail.com>
2018-02-13 01:26:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,10 +8,15 @@ class CashRegister
     @discount = discount
   end
 
+ITEMS = []
 
 
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
+    i = 0
+    while i < quantity
+      ITEMS << title
+    end
   end
 
   def apply_discount
@@ -23,9 +28,7 @@ class CashRegister
   end
 
   def items
-    what =[]
-    what << self.add_item
-    what
+
   end
 
 end
